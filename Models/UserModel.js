b271f7e549a063a6cadff75dc079b96b41d9f4a1
Kevin Freund <kevin.freund@vixlet.com>
2017-01-05 15:43:08 -0800

    - added a ".gitignore" file to the project root to prevent node.js specific files from being checked into source control (such as the node_modules f$ - modified the code so that Joe's example code works properly - added an example of a property with a getter to UserModel

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,18 +2,28 @@
  * Created by JoeShepherd on 1/5/17.
  */
 'use strict'
-class UserModel{
 
-    Constructor(firstName, lastName, email, mobilePhone){
-        this.firstname = firstName;
-        this.lastname = lastName;
-        this.email = email;
-        this.mobilephone = mobilePhone;
-    }
+class UserModel {
 
-    fullName(){
-        return `(${this.firstname}, ${this.lastname})`
-    }
+  constructor(firstName, lastName, email, mobilePhone) {
+    this.firstname = firstName;
+    this.lastname = lastName;
+    this.email = email;
+    this.mobilephone = mobilePhone;
+  }
+
+  // Example of a class property with a getter
+  // let result = model.fullNameProperty;
+  get fullNameProperty() {
+    return `(${this.firstname}, ${this.lastname})`;
+  }
+
+  // Example of a class function
+  // let result = model.fullName();
+  fullName() {
+    return `(${this.firstname}, ${this.lastname})`
+  }
+
 }
 
 module.exports = UserModel;
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,10 @@ var express = require('express');
 var router = express.Router();
 var UserModel = require('../Models/UserModel');
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var myUser = new UserModel('Joe', 'Shepherd', 'test@test.com', '555.555.1212');
-    res.render('index', {title:myUser.fullName()});
+  var myUser = new UserModel('Joe', 'Shepherd', 'test@test.com', '555.555.1212');
+  res.render('index', { title:myUser.fullNameProperty });
 });
 
 module.exports = router;
